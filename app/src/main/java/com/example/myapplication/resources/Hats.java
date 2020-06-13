2fa876ce609699f7c546c81bb78e1eafd452f676
KiTae <rlxo1235@naver.com>
2020-06-14 01:33:26 +0900

    Ui update

--- app/src/main/java/com/example/myapplication/resources/Hats.java
+++ app/src/main/java/com/example/myapplication/resources/Hats.java
@@ -14,7 +14,7 @@ public class Hats {
 
     public static final Hat SNAPBACK_BLACK = new Hat("Black Snapback", R.drawable.fragment1,
              new BigDecimal(20.99), 9377376);
-    public static final Hat SNAPBACK_CAMO = new Hat("Camo Snapback", R.drawable.fragment2,
+    public static final Hat SNAPBACK_CAMO = new Hat("Camo Snapback", R.drawable.main,
              new BigDecimal(20.99), 9377377);
     public static final Hat SNAPBACK_GREY = new Hat("Grey Snapback", R.drawable.fragment3,
              new BigDecimal(20.99), 9377378);
